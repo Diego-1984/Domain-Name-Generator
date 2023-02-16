0b8ca2674c06a10404bb328dac2d05ec2558c39e
Diego-1984 <diegomenoruceta@gmail.com>
2023-02-16 18:17:51 +0000

    Sigo sin enterarme de nada

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ window.onload = () => {
   //write your code here
   const pronoun = ["the", "our"];
   const adj = ["great", "big"];
-  const noun = ["jogger", "racoon"];
-  const domain = [".com", ".es", ".net", ".us"];
-  newDomain(pronoun, adj, noun, domain);
+  const subjet = ["jogger", "racoon"];
+  const extension = [".com", ".es", ".it", ".de"];
+  newDomain(pronoun, adj, subjet, extension);
 };
 
-function newDomain(pronoun, adj, noun, domain) {
+function newDomain(pronoun, adj, subjet, extension) {
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        for (let l = 0; l < domain.length; l++) {
-          console.log(pronoun[i] + adj[j] + noun[k] + domain[l]);
+      for (let k = 0; k < subjet.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          
+          console.log(pronoun[i] + adj[j] + subjet[k] + extension[l]);
         }
       }
     }
