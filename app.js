515f89497cc870b0ca1bc0b8d4207faaca0e229a
Diego-1984 <diegomenoruceta@gmail.com>
2023-02-19 15:32:51 +0000

    Ya se ve el resultado en la web

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ function newDomain(pronoun, adj, subjet, extension) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < subjet.length; k++) {
         for (let l = 0; l < extension.length; l++) {
-          
-          console.log(pronoun[i] + adj[j] + subjet[k] + extension[l]);
+          let div= document.createElement("div");
+          div.innerText=`${pronoun[i]}${adj[j]}${subjet[k]}${extension[l]}`
+          document.body.appendChild(div);
+                    
         }
       }
     }
